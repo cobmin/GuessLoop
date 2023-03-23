4355c4422029e28f12170d03f31a26ad4f67b543
cobmin <jacob-huber@outlook.com>
2023-03-22 19:21:39 -0500

    Initial commit

---
+++ GuessLoop/Helpers/ModifyImage.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Helpers
+{
+    public class ModifyImage
+    {
+        public static void ResizeImage(string sourceFilePath, string destFilePath, float widthCm, float heightCm)
+        {
+            using (var sourceImage = new Bitmap(sourceFilePath))
+            {
+                // Calculate the target size in pixels based on the desired centimeter dimensions and the resolution of the source image
+                float dpiX = sourceImage.HorizontalResolution;
+                float dpiY = sourceImage.VerticalResolution;
+                int widthPx = (int)(widthCm * dpiX / 2.54f);
+                int heightPx = (int)(heightCm * dpiY / 2.54f);
+
+                // Create a new bitmap with the target size
+                using (var targetImage = new Bitmap(widthPx, heightPx))
+                {
+                    using (var graphics = Graphics.FromImage(targetImage))
+                    {
+                        // Set the interpolation mode to high quality to improve the image quality
+                        graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                        // Draw the source image onto the target image, scaling it to fit within the target size
+                        graphics.DrawImage(sourceImage, new Rectangle(0, 0, widthPx, heightPx), new Rectangle(0, 0, sourceImage.Width, sourceImage.Height), GraphicsUnit.Pixel);
+                    }
+
+                    // Save the target image to the specified file path
+                    targetImage.Save(destFilePath);
+                }
+            }
+        }
+        public static void CropImage(string sourceFilePath, string destFilePath, Rectangle cropArea)
+        {
+            using (var sourceImage = new Bitmap(sourceFilePath))
+            {
+                // Create a new bitmap with the size of the crop area
+                using (var croppedImage = new Bitmap(cropArea.Width, cropArea.Height))
+                {
+                    using (var graphics = Graphics.FromImage(croppedImage))
+                    {
+                        // Draw the cropped region of the source image onto the cropped image
+                        graphics.DrawImage(sourceImage, new Rectangle(0, 0, croppedImage.Width, croppedImage.Height), cropArea, GraphicsUnit.Pixel);
+                    }
+
+                    // Save the cropped image to the specified file path
+                    croppedImage.Save(destFilePath);
+                }
+            }
+        }
+        public static List<string> GetImages(string framePath)
+        {
+            Image frame;
+            List<string> framePaths = new List<string>();
+            try
+            {
+                foreach (string imagePath in Directory.GetFiles(framePath))
+                {
+                    string extension = Path.GetExtension(imagePath);
+
+                    if (extension == ".jpg" || extension == ".png" || extension == ".gif")
+                    {
+                        framePaths.Add(imagePath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return framePaths;
+        }
+        public static void ToPrinterPaper(List<string> sourceFilePath)
+        {
+            var widthCm = 21;
+            var heightCm = 27;
+            var width = 950;
+            var height = 1251;
+            var counter = 0;
+
+            using (var bitmap = new Bitmap(widthCm, heightCm))
+            {
+                using (var canvas = Graphics.FromImage(bitmap))
+                {
+                    foreach (var item in sourceFilePath)
+                    {
+                        var image = Image.FromFile(item);
+                        canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        canvas.DrawImage(image,
+                                            new Rectangle(0,
+                                                        0,
+                                                        width,
+                                                        height),
+                                            new Rectangle(0,
+                                                        0,
+                                                        image.Width,
+                                                        image.Height),
+                                            GraphicsUnit.Pixel);
+                        canvas.Save();
+                    }
+                }
+                try
+                {
+                    bitmap.Save($".\\Img\\Output\\Pages\\Page{counter++}.png",
+                                System.Drawing.Imaging.ImageFormat.Png);
+                }
+                catch (Exception ex) { }
+            }
+        }
+    }
+}
---
+++ GuessLoop/Program.cs
@@ -0,0 +1,82 @@
+﻿using ConsoleApp1.Helpers;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+List<string> framePaths = ModifyImage.GetImages(".\\Img\\Frames");
+List<string> facePaths = ModifyImage.GetImages(".\\Img\\Faces");
+
+foreach (var item in facePaths)
+{
+    string[] substrings = item.Split(new string[] { "\\" }, StringSplitOptions.None);
+    var fileName = substrings[substrings.Length - 1].Split('.')[0];
+
+    Rectangle cropArea = new Rectangle(191, 0, 950, 1251); 
+    ModifyImage.CropImage(item, $".\\Img\\Output\\Cropped\\{fileName}.{substrings[substrings.Length - 1].Split('.')[1]}", cropArea);
+
+    var face = Image.FromFile($".\\Img\\Output\\Cropped\\{fileName}.{substrings[substrings.Length - 1].Split('.')[1]}");
+
+    using (face)
+    {
+        var counter = 0;
+        var width = 950;
+        var height = 1251;
+        foreach (var framePathx in framePaths)
+        {
+            var frame = Image.FromFile(framePathx);
+            using (var bitmap = new Bitmap(width, height))
+            {
+                using (var canvas = Graphics.FromImage(bitmap))
+                {
+                    canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    canvas.DrawImage(face,
+                                     new Rectangle(0,
+                                                   0,
+                                                   width,
+                                                   height),
+                                     new Rectangle(0,
+                                                   0,
+                                                   face.Width,
+                                                   face.Height),
+                                     GraphicsUnit.Pixel);
+                    canvas.DrawImage(frame,
+                                     new Rectangle(0,
+                                                   0,
+                                                   width,
+                                                   height),
+                                     new Rectangle(0,
+                                                   0,
+                                                   frame.Width,
+                                                   frame.Height),
+                                     GraphicsUnit.Pixel);
+                    string text = fileName; // replace with your text
+                    Font font = new Font("Calibri", 70); // replace with your font
+                    Brush brush = new SolidBrush(Color.Black); // replace with your brush color
+                    canvas.DrawString(text, font, brush, new PointF(140, 10));
+                    canvas.Save();
+                }
+                try
+                {
+                    string frameColor;
+                    if (counter == 1)
+                        frameColor = "Blue";
+                    else if (counter == 2)
+                        frameColor = "White";
+                    else
+                        frameColor = "Big";
+                    bitmap.Save($".\\Img\\Output\\FullSize\\{fileName}{frameColor}.{substrings[substrings.Length - 1].Split('.')[1]}",
+                                System.Drawing.Imaging.ImageFormat.Png);
+                    if(counter == 1|| counter == 2)
+                        ModifyImage.ResizeImage($".\\Img\\Output\\FullSize\\{fileName}{frameColor}.{substrings[substrings.Length - 1].Split('.')[1]}", $".\\Img\\Output\\Final\\Small\\{fileName}{frameColor}.{substrings[substrings.Length - 1].Split('.')[1]}", 2.85f, 4.25f);
+                    else
+                        ModifyImage.ResizeImage($".\\Img\\Output\\FullSize\\{fileName}{frameColor}.{substrings[substrings.Length - 1].Split('.')[1]}", $".\\Img\\Output\\Final\\Big\\{fileName}{frameColor}.{substrings[substrings.Length - 1].Split('.')[1]}", 5f, 7.2f);
+                    counter++;
+                }
+                catch (Exception ex) { }
+            }
+        }
+    }
+}
+
+//List<string> cardPaths = ModifyImage.GetImages(".\\Img\\Output\\Resized");
+
+//ModifyImage.ToPrinterPaper(cardPaths);
